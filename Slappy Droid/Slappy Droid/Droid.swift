e4a1c8e7116399870880da311d2addac96a4e069
Dulio Denis <dulio.denis@yahoo.com>
2016-01-11 09:22:53 -0500

    Added an SKNode extension to support update cycle

---
+++ Slappy Droid/Slappy Droid/Droid.swift
@@ -0,0 +1,9 @@
+//
+//  Droid.swift
+//  Slappy Droid
+//
+//  Created by Dulio Denis on 1/10/16.
+//  Copyright © 2016 Dulio Denis. All rights reserved.
+//
+
+import Foundation
---
+++ Slappy Droid/Slappy Droid/SKNodeUpdate.swift
@@ -0,0 +1,18 @@
+//
+//  SKNodeUpdate.swift
+//  Slappy Droid
+//
+//  Created by Dulio Denis on 1/10/16.
+//  Copyright © 2016 Dulio Denis. All rights reserved.
+//
+
+import SpriteKit
+
+extension SKNode {
+    // extend the SKNode class with the ability to tap into the update cycle
+    // only available to the Game Scene
+    
+    func update() {
+        
+    }
+}
